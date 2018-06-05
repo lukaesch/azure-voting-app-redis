47631d4adee04f11da20aec1aedbbd28ee91c92d
Lukas Schmyrczyk <schmyrczyk@gmail.com>
2018-06-05 18:17:55 +0200

    Update config-jenkins.sh

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -3,7 +3,10 @@
 # Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install oracle-java8-installer -y
 sudo apt-get install jenkins -y
 
 # Docker
@@ -22,7 +25,7 @@ sudo apt-get update && sudo apt-get install azure-cli
 
 # Kubectl
 cd /tmp/
-sudo curl -kLO https://storage.googleapis.com/kubernetes-release/release/v1.10.2/bin/linux/amd64/kubectl
+sudo curl -kLO https://storage.googleapis.com/kubernetes-release/release/v1.8.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
